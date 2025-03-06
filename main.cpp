cd1013bd40c337918343089e3d1b53315620f363
Jake Serwe <js23g@fsu.edu>
2025-03-06 10:38:38 -0500

    moved configuration to a config file instead of CLI flags

--- main.cpp
+++ main.cpp
@@ -13,55 +13,46 @@
 #include <cstddef>
 #include "compareHypotheses.h"
 #include <chrono>
+#include "inih/INIReader.h"
 
 using namespace std::chrono;
 
 int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    std::cerr << "Please pass the configuration file.\nUsage: " << argv[0] << " <config.ini>\n";
+    return 1;
+  }
   // init benchmarking
   high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
-
-  // get the file and tree names from args
-  std::string file1, file2, tree1, tree2;
-  std::string outFile = "placeholder";
-  int setCounter = 0;
-  bool verbose = false;
-  bool bestByBeam = false;  // find best overall combo by default
-  bool preserveCombos = false;
-
-  for (int i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "--help") == 0) {
-      std::cout << "Usage: " << argv[0] << " --file1 <file1.root> --tree1 <tree1> --file2 <file2.root> --tree2 <tree2>\n";
-    } else if (strcmp(argv[i], "--file1") == 0 || strcmp(argv[i], "-f1") == 0) {
-      file1 = argv[i+1];
-      setCounter++;
-    } else if (strcmp(argv[i], "--tree1") == 0 || strcmp(argv[i], "-t1") == 0) {
-      tree1 = argv[i+1];
-      setCounter++;
-    } else if (strcmp(argv[i], "--file2") == 0 || strcmp(argv[i], "-f2") == 0) {
-      file2 = argv[i+1];
-      setCounter++;
-    } else if (strcmp(argv[i], "--tree2") == 0 || strcmp(argv[i], "-t2") == 0) {
-      tree2 = argv[i+1];
-      setCounter++;
-    } else if (strcmp(argv[i], "--verbose") == 0 || strcmp(argv[i], "-v") == 0) {
-      verbose = true;
-    } else if (strcmp(argv[i], "--outfile") == 0 || strcmp(argv[i], "-o") == 0) {
-      // TODO: check that filename is valid
-      outFile = argv[i+1];
-    } else if (strcmp(argv[i], "--best-per-beam") == 0 || strcmp(argv[i], "-bb") == 0) {
-      bestByBeam = true;
-    } else if (strcmp(argv[i], "--preserve-combos") == 0 || strcmp(argv[i], "-p") == 0) {
-      preserveCombos = true;
-    }
-  }
-
-  // not enough parameters passed
-  if (setCounter != 4) {
-    std::cerr << "Missing required parameters. ";
-    std::cout << "Usage: " << argv[0] << " --file1 <file1.root> --tree1 <tree1> --file2 <file2.root> --tree2 <tree2>\n";
+  // read in config file
+  INIReader reader(argv[1]);
+  if (reader.ParseError() != 0) {
+    std::cerr << "Error reading config file " << argv[1] << '\n';
     return 1;
   }
+  
+  // read primary and secondary configs
+  std::string file1 = reader.Get("Primary", "file", "");
+  std::string file2 = reader.Get("Secondary", "file", "");
+
+  std::string tree1 = reader.Get("Primary", "tree", "");
+  std::string tree2 = reader.Get("Secondary", "tree", "");
+
+  // optional configs
+  std::string outFile = reader.Get("Misc", "outfile", "placeholder");
+  bool bestByBeam = reader.GetBoolean("Misc", "best_per_beam", false);
+  bool preserveCombos = reader.GetBoolean("Misc", "preserve_combos", false);
+  bool verbose = reader.GetBoolean("Misc", "verbose", false);
+
+  // validate config
+  if (file1.empty() || file2.empty() || tree1.empty() || tree2.empty()) {
+    std::cerr << "Missing required parameters in config file.\n";
+    std::cerr << "Required sections/keys:\n";
+    std::cerr << "[Primary]\nfile = ...\ntree = ...\n\n";
+    std::cerr << "[Secondary]\nfile = ...\ntree = ...\n";
+    return 1;
+}
 
   if (bestByBeam) {
     std::cout << "Running in best combo per beam ID mode.\n";
